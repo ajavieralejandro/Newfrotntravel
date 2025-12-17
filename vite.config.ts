41ba2c232aebc3535ff674ca8bcc0219f254cd27
ajavieralejandro <amorosijavier@gmail.com>
2025-12-17 19:56:45 +0000

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-// 🔧 Agregamos configuración de Vitest
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {},
-  },
-  json: {
-    namedExports: true,
-    stringify: false,
-  },
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'components'),
-      '@context': path.resolve(__dirname, 'context'),
-      '@hooks': path.resolve(__dirname, 'hooks'),
-      '@interfaces': path.resolve(__dirname, 'interfaces'),
-      '@services': path.resolve(__dirname, 'services'),
-      '@pages': path.resolve(__dirname, 'pages'),
-      '@data': path.resolve(__dirname, 'data'),
-      '@utils': path.resolve(__dirname, 'utils'),
-      '@assets': path.resolve(__dirname, 'assets'),
+  resolve: { /* tus alias */ },
+
+  build: {
+    manifest: true,
+    outDir: path.resolve(__dirname, "../../../../public/build-newfrotntravel"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.tsx"),
     },
   },
-  
 });
